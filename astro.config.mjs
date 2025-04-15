f82b2938a3629d581360a69660c418662c1abd64
JustStop__ <contact@juststop.dev>
2025-04-15 11:02:54 +0200

    Test with mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx'
 
 export default defineConfig({
     output: 'static',
@@ -25,6 +26,7 @@ export default defineConfig({
     integrations: [
         tailwind(),
         sitemap(),
+        mdx(),
         compress({
             CSS: true,
             SVG: false,
